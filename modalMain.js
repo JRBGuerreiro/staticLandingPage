abddc41b5d34fb45d643bb34f2284ec227387a0c
JorgeGuerreiro <crcguerreiro@gmail.com>
2021-01-17 17:12:06 +0000

    modal added, listeners attached. missing a bit of style

---
+++ modalMain.js
@@ -0,0 +1,40 @@
+//our modalInner component
+var modalInner = document.getElementById("modal-inner");
+
+//our modal component
+var modal = document.getElementById("modal")
+
+//button to open the modal
+var modalBtn = document.getElementById("signUp")
+
+//close button in modal
+var closeBtn = document.getElementById("modal-close");
+
+//Is modal open?
+var isOpen = false
+
+closeBtn.addEventListener('click', () => {
+    modalInner.style.top = '-100vh';
+    modal.style.display = 'none'
+    isOpen=false
+})
+
+window.addEventListener('click', (e) => {
+    if(isOpen && e.target == modal) {
+        modalInner.style.top = '-100vh';
+        modal.style.display = 'none'
+        isOpen = false;
+    }
+})
+
+modalBtn.addEventListener('click', () => {
+    if(!isOpen) {
+        modal.style.display = 'block';
+        modalInner.style.top = 'calc(50% - 250px)';
+        isOpen = true;
+    }
+})
+
+
+
+
